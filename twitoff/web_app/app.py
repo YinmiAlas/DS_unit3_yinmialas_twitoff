05ad25709ab0d07e0ecb4b3b2f9b5fa423e8c11f
YinmiAlas <64812727+YinmiAlas@users.noreply.github.com>
2020-10-08 19:56:37 -0700

    remove pycatch

--- twitoff/web_app/app.py
+++ twitoff/web_app/app.py
@@ -52,6 +52,8 @@ def create_app():
     def reset():
         DB.drop_all()
         DB.create_all()
+        return render_template('base.html', title='All Tweets Update!!', users=User.query.all())
+
 
     @app.route('/update', methods=['GET'])
     def update():
