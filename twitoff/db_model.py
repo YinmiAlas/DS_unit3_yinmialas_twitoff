c224dae10ea684d7b4e03711baa411b70ca53aaf
YinmiAlas <64812727+YinmiAlas@users.noreply.github.com>
2020-10-08 20:40:21 -0700

    new

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -16,7 +16,7 @@ class User(DB.Model):
 
 class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
-    tweet = DB.Column(DB.String(280), unique=False, nullable=False)
+    tweet = DB.Column(DB.String(300), unique=False, nullable=False)
     embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweet', lazy=True))
