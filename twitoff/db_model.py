c9746de54e40c10bf1c20ff9c4f97e0c22af823c
YinmiAlas <64812727+YinmiAlas@users.noreply.github.com>
2020-10-08 20:35:22 -0700

    new

--- twitoff/db_model.py
+++ twitoff/db_model.py
@@ -4,20 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
     followers = DB.Column(DB.String(120), unique=True, nullable=False)
     # tweets ids are ordinal ints, so we can fetch most recent data
     newest_tweet_id = DB.Column(DB.BigInteger, nullable=False)
 
+
     def __repr__(self):
         return '<User %r>' % self.username
 
 class Tweet(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
+    id = DB.Column(DB.BigInteger, primary_key=True)
     tweet = DB.Column(DB.String(280), unique=False, nullable=False)
     embedding = DB.Column(DB.PickleType, nullable=False)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweet', lazy=True))
    
     def __repr__(self):
